d30fdff4e848060e00da171e09ff541976be50ad
Davide Nastri <d.nastri@gmail.com>
2020-01-21 20:19:13 +0100

    Implementing ConfigurationItem

--- src/main/java/it/davidenastri/healthcheck/ConfigurationItem.java
+++ src/main/java/it/davidenastri/healthcheck/ConfigurationItem.java
@@ -2,45 +2,59 @@ package it.davidenastri.healthcheck;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.util.List;
 
-//@Entity
+@Entity
 public class ConfigurationItem {
-//    @Id
-//    @GeneratedValue
-//    private Long id;
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
     private String hostname;
     private String type;
-    private List<Check> checks;
+    //private List<Check> checks;
 
     protected ConfigurationItem() {}
 
     public ConfigurationItem(String hostname, String type, List<Check> checks) {
         this.hostname = hostname;
         this.type = type;
-        this.checks = checks;
+//        this.checks = checks;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getHostname() {
         return hostname;
     }
 
+    public void setHostname(String hostname) {
+        this.hostname = hostname;
+    }
+
     public String getType() {
         return type;
     }
 
-    public List<Check> getChecks() {
-        return checks;
+    public void setType(String type) {
+        this.type = type;
     }
 
     @Override
     public String toString() {
         return "ConfigurationItem{" +
-//                "id=" + id +
+                "id=" + id +
                 ", hostname='" + hostname + '\'' +
                 ", type='" + type + '\'' +
-                ", checks=" + checks +
+//                ", checks=" + checks +
                 '}';
     }
 
