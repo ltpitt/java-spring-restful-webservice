7ae91e8f1d14e93b76d0aa1c885f48b70b7d5916
Davide Nastri <d.nastri@gmail.com>
2020-01-24 12:32:01 +0100

    Implementing ConfigurationItemService

--- src/main/java/it/davidenastri/healthcheck/ConfigurationItemService.java
+++ src/main/java/it/davidenastri/healthcheck/ConfigurationItemService.java
@@ -1,4 +1,28 @@
 package it.davidenastri.healthcheck;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ConfigurationItemService {
+//    @Autowired
+//    ConfigurationItemRepository repository;
+//    public void add(DogDto dto) {
+//        repository.save(toEntity(dto));
+//    }
+//    public void delete(long id) {
+//        repository.deleteById(id);
+//    }
+//    public List<Dog> getDogs() {
+//        return (List<Dog>) repository.findAll();
+//    }
+//    public Dog getDogById(long id) {
+//        Optional<Dog> optionalDog = repository.findById(id);
+//        return optionalDog.orElseThrow(() -> new DogNotFoundException("Couldn't find a Dog with id: " + id));
+//    }
+//    private Dog toEntity(DogDto dto) {
+//        Dog entity = new Dog();
+//        entity.setName(dto.getName());
+//        entity.setAge(dto.getAge());
+//        return entity;
+//    }
 }
