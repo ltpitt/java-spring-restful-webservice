8f620134bfaa405261223da0f19f857273979036
Davide Nastri <d.nastri@gmail.com>
2020-01-31 12:40:52 +0100

    Implemented logging in ConfigurationItemService

--- src/main/java/it/davidenastri/healthcheck/ConfigurationItemService.java
+++ src/main/java/it/davidenastri/healthcheck/ConfigurationItemService.java
@@ -1,9 +1,27 @@
 package it.davidenastri.healthcheck;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ConfigurationItemService {
+
+    Logger logger = LoggerFactory.getLogger(ConfigurationItemService.class);
+
+    @Autowired
+    ConfigurationItemRepository repository;
+
+    public void performCheck(Check check) {
+        if (check.getProtocol().equals(Check.Protocol.HTTP)) {
+            logger.info("A DEBUG Message");
+
+        } else if (check.getProtocol().equals(Check.Protocol.HTTPS)) {
+            logger.info("Debug log HTTPS");
+        }
+    }
+
 //    @Autowired
 //    ConfigurationItemRepository repository;
 //    public void add(DogDto dto) {
