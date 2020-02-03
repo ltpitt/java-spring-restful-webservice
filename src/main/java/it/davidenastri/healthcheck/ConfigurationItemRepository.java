2f96bb27ee511fa82a43b07b808729ec4d71f7ee
Davide Nastri <d.nastri@gmail.com>
2020-02-03 21:26:27 +0100

    Implementing ConfigurationItemRepository

--- src/main/java/it/davidenastri/healthcheck/ConfigurationItemRepository.java
+++ src/main/java/it/davidenastri/healthcheck/ConfigurationItemRepository.java
@@ -1,8 +1,8 @@
 package it.davidenastri.healthcheck;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ConfigurationItemRepository extends CrudRepository<ConfigurationItem, Long> {
+public interface ConfigurationItemRepository extends JpaRepository<ConfigurationItem, Long> {
 
   ConfigurationItem findById(long id);
 
