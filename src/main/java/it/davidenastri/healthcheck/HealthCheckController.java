ce52c1b5f752a5e7909a9781b590114c06106841
Davide Nastri <d.nastri@gmail.com>
2020-02-08 20:39:59 +0100

    Implementing HealthCheckController

--- src/main/java/it/davidenastri/healthcheck/HealthCheckController.java
+++ src/main/java/it/davidenastri/healthcheck/HealthCheckController.java
@@ -2,32 +2,28 @@ package it.davidenastri.healthcheck;
 
 import kong.unirest.HttpResponse;
 import kong.unirest.JsonNode;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class HealthCheckController {
 
+    @Autowired
+    ConfigurationItemRepository configurationItemRepository;
+
+    @Autowired
+    ConfigurationItemService configurationItemService;
+
     @GetMapping("/")
-    public List<ConfigurationItem> getAllEnvironments(@RequestParam(name = "environment", required = false, defaultValue = "all") String environment) {
-        Check check = new Check(80, Check.Protocol.HTTP);
-        List<Check> checks = new ArrayList<Check>();
-        checks.add(check);
-        ConfigurationItem ci = new ConfigurationItem("davidenastri.it", "webserver", checks);
-//        HttpResponse<JsonNode> response = Utils.httpGet("http://www.mocky.io/v2/5a9ce37b3100004f00ab5154");
-//        System.out.println(environment);
-//        return response.getBody().toString();
-        List<ConfigurationItem> ciList = new ArrayList<ConfigurationItem>();
-        ciList.add(ci);
-        ciList.add(ci);
-        final ConfigurationItemService configurationItemService = new ConfigurationItemService();
-        configurationItemService.performCheck(new Check(80, Check.Protocol.HTTP));
-        return ciList;
+    public List<ConfigurationItem> getAllEnvironments() {
+        List<ConfigurationItem> cis = configurationItemRepository.findAll();
+//        cis.stream()
+//                .forEach(ci -> configurationItemService.performCheck());
+        return cis;
     }
 
     @GetMapping(value = "/api/{name}")
