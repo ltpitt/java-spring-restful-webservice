67ede66d44c40b0c137b389f310122bd85bc266c
Davide Nastri <d.nastri@gmail.com>
2020-02-29 21:46:05 +0100

    feat: Refactored ConfigurationItemService

--- src/main/java/it/davidenastri/healthcheck/ConfigurationItemService.java
+++ src/main/java/it/davidenastri/healthcheck/ConfigurationItemService.java
@@ -5,42 +5,67 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class ConfigurationItemService {
 
     Logger logger = LoggerFactory.getLogger(ConfigurationItemService.class);
 
     @Autowired
-    ConfigurationItemRepository repository;
+    ConfigurationItemRepository configurationItemRepository;
+    private List<ConfigurationItem> configurationItems = new ArrayList<ConfigurationItem>();
 
+    public ConfigurationItemService() {
+        System.out.println("Service Layer is created");
+        Check check80Http = new Check(80, Check.Protocol.HTTP);
+        Check check443Https = new Check(443, Check.Protocol.HTTPS);
+        List<Check> checks1 = new ArrayList<Check>();
+        checks1.add(check80Http);
+        List<Check> checks2 = new ArrayList<Check>();
+        checks2.add(check443Https);
+        List<Check> checks3 = new ArrayList<Check>();
+        checks3.add(check80Http);
+        checks3.add(check443Https);
+        ConfigurationItem ci = new ConfigurationItem(1, "server_1", "web_server", checks1);
+        ConfigurationItem ci2 = new ConfigurationItem(2, "server_2", "ad_server", checks2);
+        ConfigurationItem ci3 = new ConfigurationItem(3, "server_3", "db_server", checks3);
+        configurationItems.add(ci);
+        configurationItems.add(ci2);
+        configurationItems.add(ci3);
+    }
 
-    public void performCheck(Check check) {
-        if (check.getProtocol().equals(Check.Protocol.HTTP)) {
-            logger.info("A DEBUG Message");
+    public List<ConfigurationItem> getAllConfigurationItems() {
+        return configurationItems;
+    }
 
-        } else if (check.getProtocol().equals(Check.Protocol.HTTPS)) {
-            logger.info("Debug log HTTPS");
+    public ConfigurationItem getConfigurationItem(int id) {
+        return configurationItems.stream()
+                .filter(configurationItem -> configurationItem.getId() == id)
+                .findFirst().orElse(null);
+    }
+
+    public void saveConfigurationItem(ConfigurationItem configurationItem) {
+        this.configurationItems.add(configurationItem);
+    }
+
+    public void updateConfigurationItem(ConfigurationItem configurationItem) {
+        Optional<ConfigurationItem> configurationItemWithSameID = Optional.ofNullable(getConfigurationItem(configurationItem.getId()));
+        if (configurationItemWithSameID.isPresent()) {
+            ConfigurationItem configurationItemToUpdate = configurationItemWithSameID.get();
+            configurationItemToUpdate.setHostname(configurationItem.getHostname());
+            configurationItemToUpdate.setType(configurationItem.getType());
         }
     }
 
+    public void deleteConfigurationItem(int id) {
+        Optional<ConfigurationItem> configurationItemWithSameID = Optional.ofNullable(getConfigurationItem(id));
+        if (configurationItemWithSameID.isPresent()) {
+            configurationItems.remove(id);
+        }
 
-//    public void add(DogDto dto) {
-//        repository.save(toEntity(dto));
-//    }
-//    public void delete(long id) {
-//        repository.deleteById(id);
-//    }
-//    public List<Dog> getDogs() {
-//        return (List<Dog>) repository.findAll();
-//    }
-//    public Dog getDogById(long id) {
-//        Optional<Dog> optionalDog = repository.findById(id);
-//        return optionalDog.orElseThrow(() -> new DogNotFoundException("Couldn't find a Dog with id: " + id));
-//    }
-//    private Dog toEntity(DogDto dto) {
-//        Dog entity = new Dog();
-//        entity.setName(dto.getName());
-//        entity.setAge(dto.getAge());
-//        return entity;
-//    }
+    }
+
 }
