7b2330be7e7c3d1eeddf40f06ea5933be2b8fe59
Davide Nastri <d.nastri@gmail.com>
2020-03-08 09:57:53 +0100

    Removed findById from ConfigurationItemRepository

--- src/main/java/it/davidenastri/healthcheck/repository/ConfigurationItemRepository.java
+++ src/main/java/it/davidenastri/healthcheck/repository/ConfigurationItemRepository.java
@@ -1,10 +1,8 @@
 package it.davidenastri.healthcheck.repository;
 
 import it.davidenastri.healthcheck.model.ConfigurationItem;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface ConfigurationItemRepository extends JpaRepository<ConfigurationItem, Long> {
-
-  ConfigurationItem findById(long id);
+public interface ConfigurationItemRepository extends CrudRepository<ConfigurationItem, Integer> {
 
 }
